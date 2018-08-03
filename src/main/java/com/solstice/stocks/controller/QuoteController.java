3c6642a5943b4ff1f41ff17efe02a7c2957279b3
Matt Nicolay <mnicolay@solstice.com>
2018-08-03 11:48:13 -0500

    refactoring Date format-related methods to their own class: DateUtilService

--- src/main/java/com/solstice/stocks/controller/QuoteController.java
+++ src/main/java/com/solstice/stocks/controller/QuoteController.java
@@ -2,13 +2,8 @@ package com.solstice.stocks.controller;
 
 import com.solstice.stocks.data.AggregateQuote;
 import com.solstice.stocks.repository.StockRepository;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import com.solstice.stocks.service.DateUtilService;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,45 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class QuoteController {
 
-  private final Logger log = LoggerFactory.getLogger(this.getClass());
-  private final long ONE_DAY_IN_MILLISECONDS = 86400000L;
 
   private StockRepository stockRepository;
+  private DateUtilService dateUtilService;
 
-  public QuoteController(StockRepository stockRepository) {
+  public QuoteController(StockRepository stockRepository, DateUtilService dateUtilService) {
     this.stockRepository = stockRepository;
+    this.dateUtilService = dateUtilService;
   }
 
   @GetMapping("/daily/{symbol}/{dateString}")
   public AggregateQuote getAggregate(@PathVariable String symbol, @PathVariable String dateString) {
-    Date date = parseDate(dateString, "yyyy-MM-dd");
+    Date date = dateUtilService.parseDate(dateString, "yyyy-MM-dd");
 
-    return stockRepository.getAggregateData(symbol, date,
-        new Date(date.getTime() + ONE_DAY_IN_MILLISECONDS));
+    return stockRepository.getAggregateData(symbol, date, dateUtilService.getNextDay(date));
   }
 
   @GetMapping("/monthly/{symbol}/{dateString}")
   public AggregateQuote getAggregateMonthly(@PathVariable String symbol, @PathVariable String dateString) {
-    Date date = parseDate(dateString, "yyyy-MM");
+    Date fromDate = dateUtilService.parseDate(dateString, "yyyy-MM");
+    Date toDate = dateUtilService.getNextMonth(fromDate);
 
-    return stockRepository.getAggregateData(symbol, date,
-        new Date(date.getTime() + getMonthlyOffset(date)));
-  }
-
-  private Date parseDate(String dateString, String pattern) {
-    Date parsedDate = new Date();
-    try {
-      parsedDate = new SimpleDateFormat(pattern).parse(dateString);
-    } catch (ParseException e) {
-      log.error("Failed to parse date in url. Stacktrace:");
-      e.printStackTrace();
-    }
-    return parsedDate;
-  }
-
-  private long getMonthlyOffset(Date date) {
-    Calendar calendar = new GregorianCalendar();
-    calendar.setTime(date);
-    return ONE_DAY_IN_MILLISECONDS * calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+    return stockRepository.getAggregateData(symbol, fromDate, toDate);
   }
 }
---
+++ src/main/java/com/solstice/stocks/service/DateUtilService.java
@@ -0,0 +1,40 @@
+package com.solstice.stocks.service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DateUtilService {
+
+
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
+  private final long ONE_DAY_IN_MILLISECONDS = 86400000L;
+
+  public Date parseDate(String dateString, String pattern) {
+    Date parsedDate = new Date();
+    try {
+      parsedDate = new SimpleDateFormat(pattern).parse(dateString);
+    } catch (ParseException e) {
+      log.error("Failed to parse date in url. Stacktrace:");
+      e.printStackTrace();
+    }
+    return parsedDate;
+  }
+
+  public Date getNextDay(Date date) {
+    return new Date(date.getTime() + ONE_DAY_IN_MILLISECONDS);
+  }
+
+  public Date getNextMonth(Date date) {
+    Calendar calendar = new GregorianCalendar();
+    calendar.setTime(date);
+    long monthOffset = ONE_DAY_IN_MILLISECONDS * calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+    return new Date(date.getTime() + monthOffset);
+  }
+}
