1a715ff95d047330050ffbd34f62d2e9f8aef4a9
Matt Nicolay <mnicolay@solstice.com>
2018-08-07 09:04:00 -0500

    removing unnessecary Logger in DateUtilService

--- src/main/java/com/solstice/stocks/service/DateUtilService.java
+++ src/main/java/com/solstice/stocks/service/DateUtilService.java
@@ -5,15 +5,11 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DateUtilService {
 
-
-  private final Logger log = LoggerFactory.getLogger(this.getClass());
   private final long ONE_DAY_IN_MILLISECONDS = 86400000L;
 
   public Date parseDate(String dateString, String pattern) throws ParseException {
