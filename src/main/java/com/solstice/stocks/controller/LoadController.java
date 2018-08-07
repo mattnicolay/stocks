8c101a723dceff294757c3767f727f7f78c01be4
Matt Nicolay <mnicolay@solstice.com>
2018-08-07 14:38:44 -0500

    removing v1 from URL

--- src/main/java/com/solstice/stocks/controller/LoadController.java
+++ src/main/java/com/solstice/stocks/controller/LoadController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/load")
+@RequestMapping("/load")
 public class LoadController {
 
   private JsonUtilService jsonUtilService;
